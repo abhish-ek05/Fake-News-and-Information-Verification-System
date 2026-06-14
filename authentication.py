ef3e3803198f17583f082e79ad41964887af0558
abhishek <abhishpal9310@gmail.com>
2026-06-14 19:57:18 +0530

    basic registration and login file

---
+++ authentication.py
@@ -0,0 +1,37 @@
+# registration of new user 
+
+def register():
+    username = input("Enter username: ").strip()
+    password = input("Enter password: ").strip()
+    
+    # username and password validation
+
+    if username and password :
+        file = open("data/users.txt", "a")
+        file.write(username + "," + password + "\n")
+        file.close()
+
+        print("Registration Successful")
+        
+    else:
+        print("Username and Password cannot be empty")
+
+## login function ---> when correct credentials are found then login
+
+def login():
+    username = input("Enter username: ")
+    password = input("Enter password: ")
+
+    file = open("data/users.txt", "r")
+
+    for line in file:
+        user, pwd = line.strip().split(",")
+
+        if user == username and pwd == password:
+            file.close()
+            print("Login Successful")
+            return True
+
+    file.close()
+    print("Invalid Credentials")
+    return False
