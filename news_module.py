e7b15d59ba9ca73cac3121fd84b7e25f6276f949
abhishek <abhishpal9310@gmail.com>
2026-06-17 22:24:12 +0530

    updated news module , now this module can check if the news id which the user is providing is new or already present

--- news_module.py
+++ news_module.py
@@ -1,10 +1,19 @@
-## all the news is submitted through here 
-## for news submission , each one has to put news id and title
 def submit_news():
     news_id = input("Enter News ID: ")
-    title = input("Enter News Title: ")
-    file = open("dataNews.txt", "a")
-    file.write(news_id + "|" + title + "\n")
-    file.close()
+
+    with open("dataNews.txt", "a+") as file:
+        file.seek(0)
+
+        for line in file:
+            existing_id = line.strip().split("|")[0]
+
+            if existing_id == news_id:
+                print("News ID already exists!")
+                return
+
+        title = input("Enter News Title: ")
+        file.write(f"{news_id}|{title}\n")
 
     print("News Submitted Successfully")
+
+submit_news()
